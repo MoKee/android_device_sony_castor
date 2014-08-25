751fe6d547f4fb028eb9a7673f09f78404c02c3a
martincz <martincz@mokeedev.com>
2014-08-25 18:47:24 +0800

    castor: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,12 +1,12 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit MK common Phone stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/castor/full_castor.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP521/SGP521:4.4.2/17.1.1.A.0.402/bf53dw:user/release-keys PRIVATE_BUILD_DESC="SGP521-user 4.4.2 17.1.1.A.0.402 bf53dw release-keys"
 
-PRODUCT_NAME := cm_castor
+PRODUCT_NAME := mk_castor
 PRODUCT_DEVICE := castor
