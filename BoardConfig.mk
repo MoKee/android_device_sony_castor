d1c1ab46ecbba3e31b97dab4e9b962324d8c0bcf
martincz <martincz@mokeedev.com>
2014-08-25 22:59:12 +0800

    mokee bringup
    Change-Id: Id2dff7bf205a94e09f29293aa30808e2de2c2b2c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,4 +19,4 @@ include device/sony/castor_windy/BoardConfigCommon.mk
 TARGET_OTA_ASSERT_DEVICE := SGP521,SGP541,SGP551,castor
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := cm_shinano_castor_defconfig
+TARGET_KERNEL_CONFIG := mokee_shinano_castor_defconfig
